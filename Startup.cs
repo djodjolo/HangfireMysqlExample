dd04e994ea836f7455a998d651682af543793d20
Djordje Milosevic <djodjolos@gmail.com>
2019-12-24 02:27:35 +0100

    first commit

---
+++ Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Hangfire;
+using Hangfire.MySql.Core;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApplication3
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHangfire(x => x.UseStorage(new MySqlStorage("server = 127.0.0.1; uid = root; pwd =; database =hangfire; Allow User Variables = True", new MySqlStorageOptions() { TablePrefix = "hangfire" })));
+
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseHangfireServer();//启动Hangfire服务
+            app.UseHangfireDashboard();//启动hangfire面板
+            RecurringJob.AddOrUpdate("testRecurringJob", () => Console.WriteLine("Simple!"), "1 * * * * ");
+            var jobId = BackgroundJob.Enqueue(
+    () => Console.WriteLine("Fire-and-forget!"));
+            var jobIds = BackgroundJob.Schedule(
+    () => Console.WriteLine("Delayed!"),
+    TimeSpan.FromDays(7));
+            BackgroundJob.ContinueWith(
+    jobId,
+    () => Console.WriteLine("Continuation!"));
+            RecurringJob.AddOrUpdate(
+    () => Console.WriteLine("Recurring!"),
+    Cron.Minutely);
+
+        }
+    }
+}
+
+
+
+
